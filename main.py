6a87d4cf38b33a1cd3c1551a016c5671526f3f17
Kpatel323 <Patel.Kush32305@gmail.com>
2023-12-11 15:06:16 -0500

    initial

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+import requests
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/find-sports-fields', methods=['GET'])
+def get_businesses():
+    location = request.args.get('location')
+    sorting = request.args.get('option')
+
+    yelp_api_key = os.getenv("YELP_API_KEY")
+    if not yelp_api_key:
+        return jsonify({"error": "Yelp API key not provided"}), 500
+
+    url = "https://api.yelp.com/v3/businesses/search"
+    headers = {'Authorization': f'Bearer {yelp_api_key}'}
+    params = {"location": location, "term": "sports fields and facilities", "option": sorting,}
+    
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            return jsonify(data)
+        else:
+            return jsonify({"error": "Failed to fetch data from Yelp API"}), response.status_code
+    except Exception as e:
+        return jsonify({"error": f"Error making request to Yelp API: {e}"}), 500
+    
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT"))
