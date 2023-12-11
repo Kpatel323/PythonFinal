fa199bd7f74131ae2e91721773529ba41718ef6f
Kpatel323 <Patel.Kush32305@gmail.com>
2023-12-11 15:38:28 -0500

    Comments

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ CORS(app)
 
 @app.route('/api/find-sports-fields', methods=['GET'])
 def get_businesses():
+    #Get the queries from the url being sent from frontend
     location = request.args.get('location')
     sorting = request.args.get('option')
 
+    #Keep API key in a .env file for security, if it's not found then send a 500 error message
     yelp_api_key = os.getenv("YELP_API_KEY")
     if not yelp_api_key:
         return jsonify({"error": "Yelp API key not provided"}), 500
 
+    #Yelp API with authorization
     url = "https://api.yelp.com/v3/businesses/search"
     headers = {'Authorization': f'Bearer {yelp_api_key}'}
     params = {"location": location, "term": "sports fields and facilities", "option": sorting,}
@@ -28,7 +31,8 @@ def get_businesses():
             return jsonify({"error": "Failed to fetch data from Yelp API"}), response.status_code
     except Exception as e:
         return jsonify({"error": f"Error making request to Yelp API: {e}"}), 500
-    
+
+#Host frontend page
 @app.route('/')
 def home():
     return render_template('index.html')
